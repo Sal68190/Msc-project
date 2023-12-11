524185909e208b40ad9ef477d373ece4c0b8f787
Sal68190 <45146669+Sal68190@users.noreply.github.com>
2023-12-11 18:05:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 # app/app.py
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import os
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diagnosis_database.db'  # SQLite database file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Model for storing diagnosis records
+class Diagnosis(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    diagnosis_result = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+db.create_all()  # Create tables in the database
 
 model = tf.keras.models.load_model('../model/your_pretrained_model.h5')
 
@@ -24,6 +38,9 @@ def diagnose():
         predictions = model.predict(img_array)
         diagnosis_result = process_predictions(predictions)
 
+        # Save diagnosis result to the database
+        save_diagnosis_result(diagnosis_result)
+
         return jsonify({'diagnosis': diagnosis_result})
 
     except Exception as e:
@@ -34,5 +51,12 @@ def process_predictions(predictions):
     # For simplicity, return 'Normal' or 'Abnormal' based on a threshold
     return 'Normal' if predictions[0][0] > 0.5 else 'Abnormal'
 
+def save_diagnosis_result(diagnosis_result):
+    # Save diagnosis result to the database
+    new_diagnosis = Diagnosis(diagnosis_result=diagnosis_result)
+    db.session.add(new_diagnosis)
+    db.session.commit()
+
 if __name__ == '__main__':
     app.run(debug=True)
+
